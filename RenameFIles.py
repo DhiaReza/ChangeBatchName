5d23a310d181c01fc11f6cafaba4989aa945e2d4
Fahreza Dhia <fahrezadhia16@gmail.com>
2024-04-29 18:15:43 +0700

    adding some stuffs like modes and adding more extensions to check

--- RenameFIles.py
+++ RenameFIles.py
@@ -1,24 +1,56 @@
 import os
-# specify the directory path
-dir_path = "E:\Wee-A-Boo\Tales of Demons and Gods\S1"
 
+# file extensions to check
+files_extensions = [".mp4", ".mkv"]
 
+# file directory
+dir_path = ""
 
-print(os.listdir(dir_path))
+def directory():
+    global dir_path
+    
+    print("List of modes :")
+    print("1 to use current directory")
+    print("2 to use user input directory")
+    print("3 to add file extensions to check")
+    mode = input("Choose Mode : ")
+    if mode == "1":  
+        # use current directory
+        dir_path = os.getcwd()
+        return dir_path
+    elif mode == "2":
+        # user inputed directory
+        dir_path = input("Copy and Paste you directory here : ")
+        return dir_path
+    elif mode == "3":
+        # add more extensions to check
+        print(files_extensions)
+        more_ext = str(input("Input extensions here (example : .mp3) : "))
+        files_extensions.append(more_ext)
+        print(more_ext, ' has been added')
+        directory()
+    else:
+        print("wrong input!\n")
+        directory()
 
+def get_files(dir_path):
+    # get file list in a directory
+    files_names = os.listdir(dir_path)
+    return files_names
 
+# # checking which to slice
+# for x in range(0, len(files_names[1])):
+# 	print(files_names[1][x], " | ", x)
 
-# # loop through all files in the directory
-# for filename in os.listdir(dir_path):
+# #checking for every files and every file extensions
+# for filename in files_names:
+#     # check if the file is something
+#     for file_ext in range(0, len(files_extensions)):
+#         if filename.endswith(files_extensions[file_ext]):
+#             old_file_path = os.path.join(dir_path, filename)
+#             filename_new = filename[11:len(filename)]
+#             new_file_path = os.path.join(dir_path, filename_new)
+#             os.rename(old_file_path, new_file_path)
 
-#     # check if the file is a text file
-#     if filename.endswith(".txt") or filename.endswith(".mp4"):
-
-#         # construct the old file path
-#         old_file_path = os.path.join(dir_path, filename)
-
-#         # construct the new file name
-#         new_file_name = f"new_{filename}"
-
-#         # construct the new file path
-#         new_file_path = os.path.join(dir_path, new_file_name)
+directory()
+print(dir_path)
